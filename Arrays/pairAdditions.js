5b3989a997a6e9264bdfa83c593df10ce1c9aede
Pooja Junnarkar <pooja.junnarkar@copart.com>
2017-11-12 20:41:54 -0600

    pairAdditions

---
+++ Arrays/pairAdditions.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+  for (var i = 0; i < nums.length - 1; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
+
+console.log(twoSum([3, 2, 4, 1], 4))
